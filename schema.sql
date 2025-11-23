57e63f773851ba4d738a29671015953a104b624d
kristian2434 <24-34799@g.batstate-u.edu.ph>
2025-11-23 22:57:18 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE users ( user_id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), role VARCHAR(50));
 INSERT INTO users (username,role) VALUES ('Fernan.Bersuto', 'admin'), ('John.Ray', 'student');
+ALTER TABLE users ADD Email varchar(255);
